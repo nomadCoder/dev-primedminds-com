9c23708065a0e80bbec5d352fb1d40cfbe7e168d
Jody Blackwood <jody@manalogic.com>
2017-11-02 10:51:40 +1000

    Added index_new.php with adjusted structure

---
+++ includes/doc-start.php
@@ -0,0 +1,27 @@
+<html>
+
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Primed Minds</title>
+    <!-- materialize css -->
+    <link rel="stylesheet" type="text/css" href="stylesheets/materialize.min.css">
+
+    <!-- jquery js -->
+    <script src="public/javascript/jquery-1.12.3.min.js"></script>
+
+    <!-- materialize js -->
+    <script src="public/javascript/materialize.min.js"></script>
+
+    <!-- fonts from gapis -->
+    <link href="https://fonts.googleapis.com/css?family=Arima+Madurai:800" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+
+    <!-- local css -->
+    <link rel="stylesheet" type="text/css" href="stylesheets/index.css">
+
+    <script src="javascript/navbar.js"></script>
+    <script src="javascript/index.js"></script>
+    <script src="javascript/header.js"></script>
+</head>
+
+<body>
--- index_new.php
+++ index_new.php
@@ -1,7 +1,25 @@
-<html>
-<head>
-</head>
-<body>
-Starting build
-</body>
-</html>
+<?php 
+    // include the document start and standard page header
+    include '/includes/doc-start.php';
+    include '/includes/page-header.php';
+?>
+
+<!-- START: page content -->
+
+<div id="main">
+    <video playsinline autoplay muted loop id="bgvid">
+        <source src="assets/index/PMWelcomeVideo.mp4" type="video/mp4">
+    </video>
+
+    <a id="about_ref" href="about.html"><p id="homeText">A tool to inspire you mathematically!</p></a>
+
+    <a id="volume" onclick="toggleVolume()"><img id="vol_icon" src="assets/index/speaker.png" height="40px"></a>
+</div>
+
+<!-- END: page content -->
+
+<?php
+    // include the document end to close body and html
+    include '/includes/page-footer.php';
+    include '/includes/doc-end.php';
+?>    
