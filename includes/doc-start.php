106a02543f8d2dd92480b21ccfab6141e002c8be
Jody Blackwood <jody@manalogic.com>
2017-11-03 11:13:01 +1000

    Restructure of assets and pages to setup index.php as new format

--- includes/doc-start.php
+++ includes/doc-start.php
@@ -4,27 +4,27 @@
     <meta charset="UTF-8">
     <title>Primed Minds</title>
     <!-- materialize css -->
-    <link rel="stylesheet" type="text/css" href="stylesheets/materialize.min.css">
+    <link rel="stylesheet" type="text/css" href="/_css/materialize.min.css">
 
     <!-- jquery js -->
-    <script src="public/javascript/jquery-1.12.3.min.js"></script>
+    <script src="/_js/jquery-1.12.3.min.js"></script>
 
     <!-- materialize js -->
-    <script src="public/javascript/materialize.min.js"></script>
+    <script src="/_js/materialize.min.js"></script>
 
     <!-- fonts from gapis -->
     <link href="https://fonts.googleapis.com/css?family=Arima+Madurai:800" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 
     <!-- local css -->
-    <link rel="stylesheet" type="text/css" href="stylesheets/index.css">
+    <link rel="stylesheet" type="text/css" href="/_css/index.css">
 
     <!-- nav css -->
-    <link rel="stylesheet" type="text/css" href="stylesheets/navbar.css">
+    <link rel="stylesheet" type="text/css" href="/_css/navbar.css">
 
-    <script src="javascript/navbar.js"></script>
-    <script src="javascript/index.js"></script>
-    <script src="javascript/header.js"></script>
+    <script src="/_js/navbar.js"></script>
+    <script src="/_js/index.js"></script>
+    <script src="/_js/header.js"></script>
 </head>
 
 <body>
--- index.php
+++ index.php
@@ -1,54 +1,29 @@
-<!DOCTYPE html>
-<html>
+<?php 
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
 
-<head lang="en">
-    <meta charset="UTF-8">
-    <title>Primed Minds</title>
-    <!-- materialize css -->
-    <link rel="stylesheet" type="text/css" href="stylesheets/materialize.min.css">
+    // include the document start and standard page header
+    // include 'http://dev-primedminds.mybluemix.net/includes/doc-start.php';
+    include $_SERVER['DOCUMENT_ROOT'].'/includes/doc-start.php';
+    include $_SERVER['DOCUMENT_ROOT'].'/includes/page-header.php';
+?>
 
-    <!-- jquery js -->
-    <script src="public/javascript/jquery-1.12.3.min.js"></script>
+<!-- START: page content -->
 
-    <!-- materialize js -->
-    <script src="public/javascript/materialize.min.js"></script>
+<div id="main">
+    <video playsinline autoplay muted loop id="bgvid">
+        <source src="assets/index/PMWelcomeVideo.mp4" type="video/mp4">
+    </video>
 
-    <!-- fonts from gapis -->
-    <link href="https://fonts.googleapis.com/css?family=Arima+Madurai:800" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <a id="about_ref" href="about.html"><p id="homeText">A tool to inspire you mathematically!</p></a>
 
-    <!-- local css -->
-    <link rel="stylesheet" type="text/css" href="stylesheets/index.css">
+    <a id="volume" onclick="toggleVolume()"><img id="vol_icon" src="assets/index/speaker.png" height="40px"></a>
+</div>
 
-    <script src="javascript/navbar.js"></script>
-    <script src="javascript/index.js"></script>
-    <script src="javascript/header.js"></script>
-</head>
+<!-- END: page content -->
 
-<body>
-    <!-- navbar code, copy code to top of body -->
-    <div id="navbar"></div>
-    <!-- load the navbar --> 
-    <script>
-        $(function(){
-            $("#navbar").load("navbar.html"); 
-            console.log( $("index").className );
-        });
-                    
-    </script>
-
-    <div id="main">
-        <video playsinline autoplay muted loop id="bgvid">
-		    <source src="assets/index/PMWelcomeVideo.mp4" type="video/mp4">
-		</video>
-
-        <a id="about_ref" href="about.html"><p id="homeText">A tool to inspire you mathematically!</p></a>
-
-        <a id="volume" onclick="toggleVolume()"><img id="vol_icon" src="assets/index/speaker.png" height="40px"></a>
-        <footer>
-            &copy; Copyright 2016
-        </footer>
-    </div>
-</body>
-
-</html>
+<?php
+    // include the document end to close body and html
+    include $_SERVER['DOCUMENT_ROOT'].'/includes/page-footer.php';
+    require $_SERVER['DOCUMENT_ROOT'].'/includes/doc-end.php';
+?>   
