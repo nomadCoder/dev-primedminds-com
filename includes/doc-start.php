76e81ab730f95bc91e772a8d63ad254b07b1528a
Jody Blackwood <jody@manalogic.com>
2017-11-03 13:54:40 +1000

    Started work on about section

--- includes/doc-start.php
+++ includes/doc-start.php
@@ -1,8 +1,21 @@
+<?php 
+    // Check for page title additional text passed from included page
+    if (isset($pageTitle)) {
+        $pageTitle = " - " . $pageTitle;
+    } else {
+        var $pageTitle = "";
+    }
+    // Check for page additional header code passed from included page
+    if (!isset($headerAdd)) {
+        var $headerAdd = "";
+    }
+?>     
+
 <html>
 
 <head lang="en">
     <meta charset="UTF-8">
-    <title>Primed Minds</title>
+    <title>Primed Minds<?php print($pageTitle); ?></title>
 
     <!-- jquery js -->
     <script src="/_js/jquery-1.12.3.min.js"></script>
@@ -18,6 +31,7 @@
     <!-- fonts from gapis -->
     <link href="https://fonts.googleapis.com/css?family=Arima+Madurai:800" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
     <!-- local css -->
     <link rel="stylesheet" type="text/css" href="/_css/index.css">
@@ -27,6 +41,9 @@
 
     <!-- nav css -->
     <link rel="stylesheet" type="text/css" href="/_css/navbar.css">
+
+    <!-- additional header code -->
+    <?php print($headerAdd); ?>
     
 </head>
 
