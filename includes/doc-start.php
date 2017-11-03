d5f4c8b182794d7d4d4cc7f1c93a5c6335591fd1
Jody Blackwood <jody@manalogic.com>
2017-11-03 13:36:47 +1000

    Update doc-start to order header more cleanly

--- includes/doc-start.php
+++ includes/doc-start.php
@@ -3,14 +3,17 @@
 <head lang="en">
     <meta charset="UTF-8">
     <title>Primed Minds</title>
-    <!-- materialize css -->
-    <link rel="stylesheet" type="text/css" href="/_css/materialize.min.css">
 
     <!-- jquery js -->
     <script src="/_js/jquery-1.12.3.min.js"></script>
 
     <!-- materialize js -->
     <script src="/_js/materialize.min.js"></script>
+
+    <!-- page javascript files -->
+    <script src="/_js/navbar.js"></script>
+    <script src="/_js/index.js"></script>
+    <script src="/_js/header.js"></script>
 
     <!-- fonts from gapis -->
     <link href="https://fonts.googleapis.com/css?family=Arima+Madurai:800" rel="stylesheet">
@@ -19,12 +22,12 @@
     <!-- local css -->
     <link rel="stylesheet" type="text/css" href="/_css/index.css">
 
+    <!-- materialize css -->
+    <link rel="stylesheet" type="text/css" href="/_css/materialize.min.css">
+
     <!-- nav css -->
     <link rel="stylesheet" type="text/css" href="/_css/navbar.css">
-
-    <script src="/_js/navbar.js"></script>
-    <script src="/_js/index.js"></script>
-    <script src="/_js/header.js"></script>
+    
 </head>
 
 <body>
