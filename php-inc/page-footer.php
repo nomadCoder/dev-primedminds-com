2a3cbfe22d0faba099e272fd4513b8286b7d9616
Jody Blackwood <jody@manalogic.com>
2017-11-15 12:42:41 +1000

    Update footer for operations

--- php-inc/page-footer.php
+++ php-inc/page-footer.php
@@ -1,8 +1,8 @@
 <footer class="fixed-bottom">
     <div class="canvas">
-        <span class="pull-left">
+        <div class="pull-left">
             &copy; Copyright 
-        </span>
+        </div>
         <!-- Begin MailChimp Signup Form -->
         <div id="mc_embed_signup pull-right">
             <form action="https://primedminds.us15.list-manage.com/subscribe/post?u=815d9468c3345076584e0e197&amp;id=60a33df354" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate form-inline" target="_blank" novalidate>
