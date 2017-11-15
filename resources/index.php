c59aa354c44fa94ab453cd3f85aafdb1bcafff50
Jody Blackwood <jody@manalogic.com>
2017-11-15 20:25:01 +1000

    Update include directory

--- resources/index.php
+++ resources/index.php
@@ -1,12 +1,12 @@
 <?php 
-    // set page variables
-    $pageTitle = "About";
-    $jsDocumentFn = "/javascript/about.js";
-    $headerAdd = '<link rel="stylesheet" type="text/css" href="/stylesheets/about.css">';
-    // include the document start and standard page header
-    // include 'http://dev-primedminds.mybluemix.net/includes/doc-start.php';
-    include $_SERVER['DOCUMENT_ROOT'].'/includes/doc-start-basic.php';
-    include $_SERVER['DOCUMENT_ROOT'].'/includes/page-header.php';
+    // Set page variables
+    
+    // $pageTitle = ""; // Variable to append the page title "Primed Minds" with " - <pageTitle>"
+     $addHeadCode = '<style type="text/css"> body { background-color: #000; } </style>'; // Variable to add any additional code to the head for the page
+
+    // Include the document start and standard page header
+    include $_SERVER['DOCUMENT_ROOT'].'/php-inc/doc-start-basic.php';
+    include $_SERVER['DOCUMENT_ROOT'].'/php-inc/page-header.php';
 ?>
 
 <!-- START: page content -->
@@ -125,11 +125,10 @@
 
 </div>
 
-
 <!-- END: page content -->
 
 <?php
     // include the document end to close body and html
-    include $_SERVER['DOCUMENT_ROOT'].'/includes/page-footer.php';
-    require $_SERVER['DOCUMENT_ROOT'].'/includes/doc-end.php';
+    include $_SERVER['DOCUMENT_ROOT'].'/php-inc/page-footer.php';
+    require $_SERVER['DOCUMENT_ROOT'].'/php-inc/doc-end.php';
 ?>   
