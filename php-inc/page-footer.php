8d4f1e6ba3b5c6c62ee62861b35e87f98cacadf2
Jody Blackwood <jody@manalogic.com>
2017-11-15 13:55:35 +1000

    Updated footer for grid

--- php-inc/page-footer.php
+++ php-inc/page-footer.php
@@ -1,11 +1,12 @@
 
 <footer class="fixed-bottom">
-    <div class="canvas">
-        <div class="w-50 mr-auto">
+    <div class="row canvas">
+        <div class="col-4">
             &copy; Copyright 
         </div>
-        <!-- Begin MailChimp Signup Form -->
-        <div class="w-50 ml-auto">
+        <div class="col-8">
+            <div class="float-right">
+            <!-- Begin MailChimp Signup Form -->
             <div id="mc_embed_signup">
                 <form action="https://primedminds.us15.list-manage.com/subscribe/post?u=815d9468c3345076584e0e197&amp;id=60a33df354" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate form-inline" target="_blank" novalidate>
                     <label class="form-label">Subscribe to our newsletter</label>&nbsp;
@@ -14,8 +15,9 @@
                     <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_815d9468c3345076584e0e197_60a33df354" tabindex="-1" value=""></div>
                     <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn btn-secondary"></div>
                 </form>
-            </div>   
-        </div>    
-        <!--End mc_embed_signup-->
+            </div>  
+            <!--End mc_embed_signup-->
+            </div>
+        </div>  
     </div>
 </footer>
